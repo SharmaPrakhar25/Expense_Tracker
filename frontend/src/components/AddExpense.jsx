a10ae3003c7a2d4029acb6fa083f652bdd29f3df
Prakhar Sharma <prakhar.sharma2512@gmail.com>
2024-06-28 16:49:09 +0530

    feat: added api implementation

--- frontend/src/components/AddExpense.jsx
+++ frontend/src/components/AddExpense.jsx
@@ -4,23 +4,39 @@ import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import DatePicker from 'react-datepicker';
 import Button from 'react-bootstrap/Button';
-
+import axios from 'axios';
 import 'react-datepicker/dist/react-datepicker.css';
+
+// eslint-disable-next-line import/no-unresolved
+import { terminal } from 'virtual:terminal';
 
 function AddExpense() {
   const [selectedDate, setSelectedDate] = useState(null);
   const [category, setCategory] = useState('');
   const [price, setPrice] = useState('');
 
-  function handleSubmit(e) {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const formData = {
-      category,
-      price,
-      selectedDate,
-    };
-    console.log(formData);
-  }
+    try {
+      const expenseData = {
+        user_id: 1,
+        category,
+        amount: parseInt(price, 2),
+        // selectedDate,
+        is_shared: false,
+      };
+      terminal.log(import.meta.env);
+      await axios.post(
+        `${import.meta.env.VITE_BACKEND_BASE_URL}/expense/add`,
+        // eslint-disable-next-line comma-dangle
+        expenseData
+      );
+      // const { message } = response;
+      // Todo: show message back in toast bar
+    } catch (error) {
+      throw new Error('something went wrong');
+    }
+  };
 
   return (
     <Container>
