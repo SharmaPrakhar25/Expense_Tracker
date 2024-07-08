1d48c7344257eec99a6e2004b614ec85db927547
Prakhar Sharma <prakhar.sharma2512@gmail.com>
2024-07-09 00:00:21 +0530

    fix: fixed docker-compose not working in windows system

--- backend/dist/startup.sh
+++ backend/dist/startup.sh
@@ -1,17 +1,7 @@
-#!/bin/bash
-
-# Install dependencies
-npm install
-
-# Install Prisma globally
-npm install -g @prisma/cli
-
-# Set PATH environment variable
-export PATH=$PATH:/usr/local/bin
-
-# Run Prisma commands
-npx prisma migrate deploy
+#!/bin/sh
 npx prisma generate
+npx prisma migrate dev
 
+npx prisma db seed
 node ./dist/server.js
 
